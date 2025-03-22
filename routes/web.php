f543b76f26a7f003437008816d762a07e2b5ab19
luis <aprilluis.balbuena122616@gmail.com>
2025-03-22 22:39:59 +0800

    Edit Routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 // Route::get('/', function () {
 //     return Inertia::render('Welcome', [
