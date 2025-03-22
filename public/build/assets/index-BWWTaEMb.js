34b804709c06f79393c0db0b89c5821d227a12b1
luis <aprilluis.balbuena122616@gmail.com>
2025-03-22 22:43:01 +0800

    Edit Routes

--- public/build/assets/index-BWWTaEMb.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-Cvy3v5Vi.js";import"./app-BD6jbWMQ.js";var d=a;export{d as default};
---
+++ public/build/assets/index-jZGHPSPi.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-DJekAipv.js";import"./app-KLUfSYzo.js";var d=a;export{d as default};
